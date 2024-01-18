f7f4076bda24033eb715ca25cd22f08334d2b346
mmagdalenaa1 <borkomagdalena3@gmail.com>
2024-01-18 15:02:02 +0100

    dz

---
+++ zadaće/index.js
@@ -0,0 +1,8 @@
+var uneseniBroj = prompt ("Unesite broj:")
+
+    if (uneseniBroj > 10){
+        alert("Broj je veći od 10");
+    }
+    if(uneseniBroj < 10){
+        alert("Broj nije veći od 10");
+    }
